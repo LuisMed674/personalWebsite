073739537c45ab85993359bfa1adab8097920528
LuisMed674 <123563314+LuisMed674@users.noreply.github.com>
2024-07-16 19:59:21 -0400

    supporting CSS file

---
+++ script.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".multiple-text", {
+                                        strings: ["Frontend Developer", "Student", "Joy to be around 😊", "Software Engineer"],
+                                        typeSpeed: 100,
+                                        backSpeed: 175,
+                                        backDelay: 100,
+                                        loop: true
+})
+
+var typed = new Typed(".greeting-text", {
+                                        strings: ["Hello, I am", "Hola yo soy", "你好，我是", "Je m'appelle", "Ciao io sono", "私は"],
+                                        typeSpeed: 100,
+                                        backSpeed: 175,
+                                        backDelay: 100,
+                                        loop: true
+}
+)
+
